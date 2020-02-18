1d1cbdb4c3b32d09f8f16f2517b885d4d3203ae4
timo07 <58931126+timo07@users.noreply.github.com>
2020-02-18 19:02:53 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -62,4 +62,82 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message-uppre">` +
+          `<div class="message-uppre__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message-uppre__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message-text">` +
+          `<p class="message-text__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="message-text__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message-uppre">` +
+          `<div class="message-uppre__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message-uppre__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message-text">` +
+          `<p class="message-text__content">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message-uppre">` +
+          `<div class="message-uppre__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message-uppre__date">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message-text">` +
+          `<img src="` + message.image + `" class="message-text__image" >` +
+        `</div>` +
+      `</div>`
+    };
+    return html;
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
